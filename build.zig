308520bf63c0ee163665cae3cd77f6a208a0c256
Nedas <nedas.pranskunas@gmail.com>
2025-04-17 22:58:58 +0300

    Learning about neural networks.

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const wasm = b.resolveTargetQuery(.{
+        .cpu_arch = .wasm32,
+        .os_tag = .freestanding,
+    });
+
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = wasm,
+        .optimize = optimize,
+    });
+
+    const lib = b.addExecutable(.{
+        .name = "ai_nn",
+        .root_module = lib_mod,
+    });
+
+    lib.entry = .disabled;
+    lib.rdynamic = true;
+
+    b.installArtifact(lib);
+
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
